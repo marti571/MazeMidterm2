5b37ca8c99e3f134323f1b5b09b05b1a4b54707e
Mariana <marti571@cougars.csusm.edu>
2018-10-30 09:31:35 -0700

    first commit

---
+++ Assets/SampleScenes/Scripts/CoinCollector.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinCollector : MonoBehaviour {
+
+    public AudioClip coin;
+    AudioSource aScorce;
+    GameObject obj;
+
+    GameManagerScript GMS;
+    // Use this for initialization
+    void Awake()
+    {
+        GMS = GameObject.Find("GameManager").GetComponent<GameManagerScript>();
+        GMS.cur_coins++;
+    }
+
+    void Start () {
+        obj = GameObject.Find("AudioObject");
+        if (obj != null)
+        {
+            aScorce = obj.GetComponent<AudioSource>(); // get component once @ Start more efficient.
+          
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    
+	}
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "midtermcharacter")
+        {
+            aScorce.clip = coin; //once the coin is destroyed, make coin sound
+            aScorce.Play();
+            Destroy(gameObject);
+            GMS.cur_coins--;
+            GMS.UpdateUI();
+
+        }
+    }
+}
