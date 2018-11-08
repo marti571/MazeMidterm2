ffb3663b7a919a92261d962787277ad6d88c39b3
Mariana <marti571@cougars.csusm.edu>
2018-11-07 17:13:29 -0800

    Revert "Revert "minion movement""
    This reverts commit 87d76cca136a247317611aafdf1ad394684bf572.

---
+++ Assets/SampleScenes/Scripts/MinionBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionBehavior : MonoBehaviour {
+
+	Transform player;
+	UnityEngine.AI.NavMeshAgent nav;
+	// Use this for initialization
+	void Awake () {
+		//Find the player
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		nav.SetDestination (player.position);
+	}
+}
