fb9375eb1f203d801450bf5ccef217947101de14
Mariana <marti571@cougars.csusm.edu>
2018-11-08 22:50:33 -0800

    PlayerHealth has been added
    I could figure out how to change the color of an image to display once the character gets hurt. So if someone could please help me out with that would be much appreciated. It also needs sounds in warriorhealth.cs

---
+++ Assets/Scripts/ThirdCharacterCamera.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdCharacterCamera : MonoBehaviour {
+	[SerializeField]
+	private Transform target;
+	[SerializeField]
+	private Vector3 offsetPosition;
+	[SerializeField]
+	private Space offsetPositionspace = Space.Self;
+	[SerializeField]
+	private bool lookat = true;
+
+	private void LateUpdate()
+	{
+		Refresh();
+	}
+	public void Refresh()
+	{
+		if(target == null)
+		{
+			//Debug.LogWarning("Missing target ref !", this);
+			return;
+		}
+		if(offsetPositionspace == Space.Self)
+		{
+			transform.position = target.TransformPoint(offsetPosition);
+		}
+		else
+		{
+			transform.position = target.position + offsetPosition;
+		}
+		if (lookat)
+		{
+			transform.LookAt(target);
+		}
+		else
+		{
+			transform.rotation = target.rotation;
+		}
+	}
+	// public GameObject player;
+	// private Vector3 offset;
+	// // Use this for initialization
+	// void Start () {
+	// 	offset = transform.position - player.transform.position;
+	// }
+	
+	// // Update is called once per frame
+	// void LateUpdate () {
+	// 	transform.position = player.transform.position + offset;
+	// 	transform.LookAt(player.transform);
+	// 	//transform.rotation = target.rotation;
+	// }
+}
---
+++ Assets/Scripts/WarriorHealth.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+//using UnityEngine.CoreModule;
+
+
+public class WarriorHealth : MonoBehaviour {
+	//Health variables
+	public int maxHealth = 100;
+	public int currentHealth;
+	public Slider healthSlider;
+	//public Image damageImage = GameObject.Find("damage").GetComponent<Image>();
+	//Color newColor = damageImage.color;
+
+
+	//add a death audio public AudioClip deathClip;
+	public float flashSpeed = 5f;
+	
+
+	Animator anim;
+	CharacterBehavior characterBehavior;
+	bool isDead;
+	bool damaged;
+	// Use this for initialization
+	void Awake () {
+		//setting up the references
+		anim = GetComponent <Animator> ();
+		characterBehavior = GetComponent <CharacterBehavior> ();
+
+		//Set initial health of player
+		currentHealth = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if the player has just been damaged
+		if(damaged)
+		{
+			//set the color of damageImage
+			//damageImage.color = newColor;
+		}
+		else
+		{
+			//back to clear
+			//damageImage.color = Color.Lerp (damageImage.Color, Color.clear, flashSpeed * Time.deltaTime);
+		}
+		//reset damage flag
+		damaged = false;
+	}
+
+	public void TakeDamage (int amount)
+	{
+		//Set the damaged flag so the screen will flash
+		damaged = true;
+
+		//reduce health
+		currentHealth -= amount;
+
+		//set the health bar's value to current health
+		healthSlider.value = currentHealth;
+
+		//Play hurt sound effect
+
+		if(currentHealth <= 0 && !isDead)
+		{
+			Death();
+		}
+	}
+	void Death()
+	{
+		//set flag so this fuunction wont be called again
+		isDead = true;
+		anim.SetTrigger("Death");
+
+		//set audiosource to play death clip to stop sound of hurt
+
+		//stop the charcter from moving or punching
+		characterBehavior.enabled = false;
+	}
+}
