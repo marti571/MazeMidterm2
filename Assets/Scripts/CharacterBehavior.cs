cd825716ebefe09be43e40ebcb8deeb38228f539
Mariana <marti571@cougars.csusm.edu>
2018-11-14 18:14:18 -0800

    pause button

--- Assets/Scripts/CharacterBehavior.cs
+++ Assets/Scripts/CharacterBehavior.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class CharacterBehavior : MonoBehaviour {
 	private Animator anim;
 	private bool PlayerClicked;
@@ -10,18 +11,40 @@ public class CharacterBehavior : MonoBehaviour {
 	public int damagePerKick = 30;
 	public float timeBetweenHits = 0.15f;
 	float timer;
+	//minion parameters
 	MinionHealth minionHealth;
 	bool minionInRange;
 	GameObject minion;
+	//pause control parameters
+	private int count;
+	public bool paused;
+	GameObject[] pausedObjects;
 	// Use this for initialization
-	void Awake () {
-		minion = GameObject.FindGameObjectWithTag ("Minion");
+	void Start () {
+		Time.timeScale = 1;
+		pausedObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
+		hidePaused();
+		minion = GameObject.FindGameObjectWithTag ("Minion");	
 		minionHealth = minion.GetComponent <MinionHealth> ();
 		anim = GetComponent<Animator> ();
 	} 
 	
 	// Update is called once per frame
 	void Update () {
+		if(Input.GetKeyDown(KeyCode.P))
+		{
+			if(Time.timeScale == 1)
+			{
+				Time.timeScale = 0;
+				showPaused();
+				//onClick();
+			}
+			else if(Time.timeScale == 0)
+			{
+				Time.timeScale = 1;
+				hidePaused();
+			}
+		}
 		timer += Time.deltaTime;
 		//Walking Animation
 		anim.SetFloat("speed", Input.GetAxis("Vertical"));
@@ -31,6 +54,31 @@ public class CharacterBehavior : MonoBehaviour {
 		
 		Punching();
 		Kicking();
+	}
+	public void pauseControl()
+	{
+		if(Time.timeScale == 1)
+		{
+			Time.timeScale = 0;
+			showPaused();
+			paused=true;
+		}
+		else if(Time.timeScale == 0)
+		{
+			Time.timeScale = 1;
+			hidePaused();
+			paused=false;
+		}
+	}
+	public void showPaused(){
+		foreach(GameObject g in pausedObjects){
+			g.SetActive(true);
+		}
+	}
+	public void hidePaused(){
+		foreach(GameObject g in pausedObjects){
+			g.SetActive(false);
+		}
 	}	
 	void Punching() {
 		if(Input.GetButtonDown("Fire1"))
