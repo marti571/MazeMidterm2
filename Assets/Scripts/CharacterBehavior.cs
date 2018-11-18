0a8d64e504ce6999e73705efeedcb6aaef4c097e
Krishna <krishnakher73@gmail.com>
2018-11-17 17:14:46 -0800

    Merge branch 'master' into Riddle
    I have added riddle

--- Assets/Scripts/CharacterBehavior.cs
+++ Assets/Scripts/CharacterBehavior.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class CharacterBehavior : MonoBehaviour {
 	private Animator anim;
 	private bool PlayerClicked;
@@ -16,8 +17,10 @@ public class CharacterBehavior : MonoBehaviour {
 	public int damagePerKick = 30;
 	public float timeBetweenHits = 0.15f;
 	float timer;
+	//minion parameters
 	MinionHealth minionHealth;
 	bool minionInRange;
+<<<<<<< HEAD
 	public GameObject minion;
 
 
@@ -52,6 +55,39 @@ public class CharacterBehavior : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
+=======
+	GameObject minion;
+	//pause control parameters
+	private int count;
+	public bool paused;
+	GameObject[] pausedObjects;
+	// Use this for initialization
+	void Start () {
+		Time.timeScale = 1;
+		pausedObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
+		hidePaused();
+		minion = GameObject.FindGameObjectWithTag ("Minion");	
+		minionHealth = minion.GetComponent <MinionHealth> ();
+		anim = GetComponent<Animator> ();
+	} 
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.P))
+		{
+			if(Time.timeScale == 1)
+			{
+				Time.timeScale = 0;
+				showPaused();
+				//onClick();
+			}
+			else if(Time.timeScale == 0)
+			{
+				Time.timeScale = 1;
+				hidePaused();
+			}
+		}
+>>>>>>> master
 		timer += Time.deltaTime;
 		//Walking Animation
 		anim.SetFloat("speed", Input.GetAxis("Vertical"));
@@ -61,6 +97,31 @@ public class CharacterBehavior : MonoBehaviour {
 		
 		Punching();
 		Kicking();
+	}
+	public void pauseControl()
+	{
+		if(Time.timeScale == 1)
+		{
+			Time.timeScale = 0;
+			showPaused();
+			paused=true;
+		}
+		else if(Time.timeScale == 0)
+		{
+			Time.timeScale = 1;
+			hidePaused();
+			paused=false;
+		}
+	}
+	public void showPaused(){
+		foreach(GameObject g in pausedObjects){
+			g.SetActive(true);
+		}
+	}
+	public void hidePaused(){
+		foreach(GameObject g in pausedObjects){
+			g.SetActive(false);
+		}
 	}	
 	void Punching() {
         if(Input.GetKeyDown("fire1"))
