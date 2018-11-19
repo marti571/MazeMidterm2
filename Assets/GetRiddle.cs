183241e23c05fda90fdd4e9a0a0e9c7c9ed43406
Krishna <krishnakher73@gmail.com>
2018-11-18 22:57:20 -0800

    Riddle changes

---
+++ Assets/GetRiddle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetRiddle : MonoBehaviour {
+
+    public GameObject getRiddle;
+    public GameObject Riddle;
+	void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            getRiddle.SetActive(true);
+        }
+    }
+
+   public void OnRiddle()
+   {
+        getRiddle.SetActive(false);
+        Riddle.SetActive(true);
+    }
+    
+}
---
+++ Assets/level1/RiddleAns.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+
+
+public class RiddleAns : MonoBehaviour {
+
+    public GameObject minion;
+    public GameObject riddle;
+    public GameObject findMinion;
+    public GameObject levelCompleted;
+  
+    public void TrueAns()
+    {
+        riddle.SetActive(false);
+        levelCompleted.SetActive(true);
+
+    }
+    public void WrongAns()
+    {
+        riddle.SetActive(false);
+        findMinion.SetActive(true);
+       
+    }
+    public void GoToLevel(string gameLevel)
+    {
+        levelCompleted.SetActive(true);
+        SceneManager.LoadScene(gameLevel);
+    }
+    public void GetMinion()
+    {
+        findMinion.SetActive(false);
+        minion.SetActive(true);
+    }
+}
+
+
+   
+    
